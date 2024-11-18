2f9393a26607880aed25a61da9212c7a8af0745d
greenashberry <rayana050605@gmail.com>
2024-11-18 23:25:11 +0300

    FIFO done (not completely)
    Закончено FIFO, пока что не протестировано

--- Hometask_Project/Hometask_Project/FIFOCache.h
+++ Hometask_Project/Hometask_Project/FIFOCache.h
@@ -5,6 +5,7 @@
 #include <string>
 #include <unordered_set>
 #include <ios>
+#include <list>
 
 
 template <typename T>
@@ -24,10 +25,19 @@ public:
 
 	bool FindInFile(std::string filename, T value); //поиск в файле
 	bool FindInCache(const std::list<T>& cache, T value); //поиск в кэше
-	void ReplaceInCache(T value, int id, std::string& filename, std::list<T>& cache); //вытенения из кэша в файл и замена на новое значение
+	void ReplaceInCache(T value, int id, std::string& filename, std::list<T>& cache); //вытеснение из кэша в файл и замена на новое значение
 	bool CheckIfProcessed(T value, const std::unordered_set<T>& processed_data); //проверка на то, было ли вообще обработан этот объект
-	void WriteInFile(T value, std::string filename);
+	void CacheFilling(std::list<T>& data, T value, int size, int& id, std::string filename);
+	void Processing(FIFOCache& FIFO);
 };
+
+template<typename T>
+inline FIFOCache<T>::FIFOCache(std::list<T> datalist, int s, std::string name)
+{
+	data = datalist;
+	size = s;
+	filename = name;
+}
 
 template<typename T>
 inline bool FIFOCache<T>::FindInFile(std::string filename, T value)
@@ -43,7 +53,100 @@ inline bool FIFOCache<T>::FindInFile(std::string filename, T value)
 	{
 		if (search == value)
 		{
+			file.close();
 			return 1;
 		}
 	}
 }
+
+template<typename T>
+inline bool FIFOCache<T>::FindInCache(const std::list<T>& cache, T value)
+{
+	if (cache.empty())
+	{
+		std::cout << "Cache is empty" << std::endl;
+		return 0;
+	}
+	for (const auto& search : cache)
+	{
+		if (search == value)
+		{
+			return 1;
+		}
+	}
+}
+
+template<typename T>
+inline void FIFOCache<T>::ReplaceInCache(T value, int id, std::string& filename, std::list<T>& cache)
+{
+	std::ifstream file(filename, std::ios::app);
+	file << cache[id] << std::endl;
+	file.close();
+	cache[id] = value;
+}
+
+template<typename T>
+inline bool FIFOCache<T>::CheckIfProcessed(T value, const std::unordered_set<T>& processed_data)
+{
+	if (processed_data.contains(value))
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+template<typename T>
+inline void FIFOCache<T>::CacheFilling(std::list<T>& cache, T value, int size, int& id, std::string filename)
+{
+	if (data.size() == size)
+	{
+		ReplaceInCache(value, filename, cache);
+		id = ((id + 1) == size) ? 0: id + 1;
+	}
+	else
+	{
+		cache.push_back(value);
+		id++;
+	}
+}
+
+template<typename T>
+inline void FIFOCache<T>::Processing(FIFOCache& FIFO)
+{
+	auto start = std::chrono::high_resolution_clock::now();
+	int id = 0;
+	for (const auto& s : FIFO.data)
+	{
+		if (CheckIfProcessed(s, FIFO.processed_data))
+		{
+			if (FindInCache(FIFO.cache, s))
+			{
+				FIFO.hits++;
+			}
+			else
+			{
+				FindInFile(filename, s);
+				FIFO.misses++;
+				CacheFilling(FIFO.cache, s, FIFO.size, id, FIFO.filename);
+			}
+		}
+		else
+		{
+			CacheFilling(FIFO.cache, s, FIFO.size, id, FIFO.filename);
+			processed_data.insert(value);
+		}
+	}
+	auto end = std::chrono::high_resolution_clock::now();
+	auto overall_time = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+
+	std::cout << "Processing time: " << overall_time.count() << std::endl;
+	std::cout << "Cache hits: " << FIFO.hits << std::endl;
+	std::cout << "Cache misses " << FIFO.misses << std::endl;
+}
+
+
+
+
--- Hometask_Project/Hometask_Project/Hometask_Project.cpp
+++ Hometask_Project/Hometask_Project/Hometask_Project.cpp
@@ -1,8 +1,10 @@
-﻿
-
-#include <iostream>
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <list>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
 }
